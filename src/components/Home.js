e089c9c53e0006f169e07df573f00ca3aee11a20
Tina <tinzzy.cracked@yahoo.com>
2023-12-07 09:35:40 -0500

    getting the message and cyhange the component when board is ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,31 @@ import React, { Component } from 'react';
 import { Box } from '@mui/material';
 
 import InitForm from './InitForm';
+import Board from './Board';
+
+import { eventEmitter } from './InitForm';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            displayBoard: false
         };
+    }
+
+    componentDidMount() {
+        eventEmitter.on('boardReady', (data) => {
+            if (data.data.generation >= 0) {
+                this.setState({ displayBoard: true })
+            }
+        });
     }
 
 
     render() {
         return (
             <Box>
-                <InitForm />
+                {this.state.displayBoard ? <Board /> : <InitForm />}
             </Box>
         );
     }
