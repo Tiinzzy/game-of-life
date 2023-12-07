da4dd1803b6ee960a69da42eb24411707aece32e
Tina <tinzzy.cracked@yahoo.com>
2023-12-07 10:32:52 -0500

    added axios to post data for updating the grid

--- src/components/BackEndConnection.js
+++ src/components/BackEndConnection.js
@@ -35,6 +35,23 @@ class BackEndConnectionImpl {
             })
     }
 
+    async update_grid(query, callback) {
+        return axios.post('/gol/update', query, { headers: { 'Content-Type': 'application/json' } })
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
+
 }
 
 export default class BackEndConnection {
