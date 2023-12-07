3bb999e262dcd18412449221497d835997df51be
Tina <tinzzy.cracked@yahoo.com>
2023-12-07 09:20:59 -0500

    axios connection to backend to post data

---
+++ src/components/BackEndConnection.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+class BackEndConnectionImpl {
+    async game_of_life_init(query, callback) {
+        return axios.post('/gol/init', query, { headers: { 'Content-Type': 'application/json' } })
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
+
+}
+
+export default class BackEndConnection {
+    static #object = null;
+
+    static INSTANCE() {
+        if (BackEndConnection.#object === null) {
+            BackEndConnection.#object = new BackEndConnectionImpl();
+        }
+        return BackEndConnection.#object;
+    }
+}
